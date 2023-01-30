5dd8923fc3b83d580814812969df0c116db3ed75
sarahsidek <68525964+sarahsidek@users.noreply.github.com>
2023-01-30 16:54:21 +0800

    initial setup

---
+++ src/components/home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import { FormText } from "react-bootstrap/FormText";
+import {input} from "react"
+
+const Home = () => {
+    return (
+        <div className="text-center">
+            <h3>Todo List Sarah</h3>
+            <div>
+               {/* <FormText></FormText> */}
+            </div>
+        </div>
+    )
+}
+
+export default Home
