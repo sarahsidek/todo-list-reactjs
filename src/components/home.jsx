d59165249f15eaef0af36b207400ac051319731f
sarah <68525964+sarahsidek@users.noreply.github.com>
2023-04-08 14:46:45 +0800

    latest

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Col, Row, } from "react-bootstrap";
 import { Form } from "react-bootstrap/Form";
 
 
@@ -7,6 +8,14 @@ const Home = () => {
         <div className="text-center">
             <h3 className="big-title">Todo List Sarah</h3>
             <div>
+                <Row>
+                    <Col sm={4} xs={6}>
+                       <p>Hello</p>
+                    </Col>
+                    <Col sm={4} xs={6}>
+                       <p>Hello</p>
+                    </Col>
+                </Row>
             </div>
         </div>
     )
